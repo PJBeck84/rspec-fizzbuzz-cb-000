8573f8b14095c137f80cc81d41edb68ba2fa7d34
Patrick Beck <patrickjbeck84@gmail.com>
2020-11-21 18:18:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if fizzbuzz(number).remainder(3) == 0
+    puts "Fizz"
+end
